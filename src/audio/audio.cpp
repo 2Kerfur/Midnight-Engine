642e2ba9092399cf35bc2c366d04bd6a4446ae49
Biner <fidotovm06@gmail.com>
2022-06-06 20:39:01 +0400

    Update audio.cpp

--- src/audio/audio.cpp
+++ src/audio/audio.cpp
@@ -1,7 +1,10 @@
 #include "audio.h"
+
+#ifdef _WIN32 //TODO: Create linux audio engine implementation
 #include <Windows.h>
 #pragma comment(lib, "winmm.lib")
 void Audio::EnginePlaySound(char sound[])
 {
     PlaySound(TEXT(sound), NULL, SND_FILENAME | SND_ASYNC);
 }
+#endif
